0c06c0ef090b62ce305c14577ca77f2962235a6c
Pedro Martins <mourao.martins@gmail.com>
2025-07-12 16:52:57 +0100

    Create goalsAPI.js

---
+++ public/goalsAPI.js
@@ -0,0 +1,25 @@
+// firebaseApi.js
+import { db } from './firebase-init.js';
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js';
+
+const FirebaseAPI = {
+  async fetchGoals(user) {
+    if (!user) return [];
+    const ref = collection(db, 'users', user.uid, 'goals');
+    const snap = await getDocs(ref);
+    return snap.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  },
+  async addGoal(user, data) {
+    const ref = collection(db, 'users', user.uid, 'goals');
+    await addDoc(ref, data);
+  },
+  async updateGoal(user, id, data) {
+    const ref = doc(db, 'users', user.uid, 'goals', id);
+    await updateDoc(ref, data);
+  },
+  async deleteGoal(user, id) {
+    const ref = doc(db, 'users', user.uid, 'goals', id);
+    await deleteDoc(ref);
+  }
+};
+export default GoalsAPI;
